e53375142bd27cb58819e1fb3f15b7f3ebc5c1a5
Igor Kozlov <gofman999@mail.ru>
2022-01-30 20:10:08 +0300

    добавил файлы gitignore и README.md

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует массив 
+
+string[] array = { "кря", "привет", "123", ":-)" };
