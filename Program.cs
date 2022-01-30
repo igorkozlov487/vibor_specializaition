2e98c7a8178b47df068d302dc3b0b12b9e3734dc
Igor Kozlov <gofman999@mail.ru>
2022-01-30 20:32:56 +0300

    final commit

--- Program.cs
+++ Program.cs
@@ -2,3 +2,13 @@
 
 string[] array = { "кря", "привет", "123", ":-)" };
 string[] newarray = new string[0];
+for (int i = 0; i < array.Length; i++)
+{
+    int j = 0;
+    if (array[i].Length < 4)
+    { 
+        Array.Resize(ref newarray, j + 1);
+        newarray[j] = array[i];
+        Console.Write($"{newarray[j]}; ");
+    }
+} 
